566aff4188f67808163b83010871f66fbe03de16
Tesfahun03 <tesaf1035@gmail.com>
2025-02-07 17:46:03 +0300

    modifying ipmapping module

--- src/ip_geolocation.py
+++ src/ip_geolocation.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from intervaltree import IntervalTree
-import logging, sys, os
+import logging
+import sys
+import os
 
 # Add the src directory to the path
 sys.path.append(os.path.abspath(''))
@@ -11,50 +13,35 @@ logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
     filename='../logs/ip_geolocation.log')
 
-logging.info('****************************Logging started for IP Geolocation module****************************')
-class IPGeolocation:
-    """
-    A class to map IP addresses to their respective countries using an interval tree.
-    Attributes:
-    -----------
-    tree : IntervalTree
-        An interval tree that stores IP ranges and their corresponding countries.
-    Methods:
-    --------
-    __init__(df_ranges):
-        Initializes the IPGeolocation instance with IP ranges and builds the interval tree.
-    map_ips_to_countries(df_ips):
-        Maps a DataFrame of IP addresses to their respective countries.
-    """
+logging.info(
+    '****************************Logging started for IP Geolocation module****************************')
 
+
+class IPGeolocation:
     def __init__(self, df_ranges):
-        logging.info("Initializing IPGeolocation with IP ranges.")
-        self.tree = IntervalTree()
-        for _, row in df_ranges.iterrows():
-            self.tree[row['lower_ip']:row['upper_ip'] + 1] = row['country']
-        logging.info(
-            "Interval tree built successfully with %d ranges.", len(df_ranges))
+        try:
+            logging.info("Initializing IPGeolocation with IP ranges.")
+            self.tree = IntervalTree()
+            for _, row in df_ranges.iterrows():
+                self.tree[row['lower_bound_ip_address']
+                    :row['upper_bound_ip_address'] + 1] = row['country']
+            logging.info(
+                "Interval tree built successfully with %d ranges.", len(df_ranges))
+        except Exception as e:
+            logging.error("Error initializing IPGeolocation: %s", str(e))
+            raise
 
     def map_ips_to_countries(self, df_ips):
-        """
-        Maps IP addresses to their corresponding countries.
-        This function takes a DataFrame containing IP addresses and maps each IP
-        address to its corresponding country using an IP-to-country mapping tree.
-            df_ips (pd.DataFrame): A DataFrame containing IP addresses with at least
-                                   the following columns:
-                                   - 'ip': The IP address as a string.
-                                   - 'company': The company associated with the IP.
-                                   - 'action': The action associated with the IP.
-            pd.DataFrame: A DataFrame with the original columns plus an additional
-                          'country' column indicating the country corresponding to
-                          each IP address
-        """
-        logging.info(
-            "Mapping IPs to countries for %d IP addresses.", len(df_ips))
-        df_ips['ip_int'] = df_ips['ip'].astype(int)
-        df_ips['country'] = df_ips['ip_int'].apply(lambda ip: next(
-            iter(self.tree[ip])).data if self.tree[ip] else None)
-        matched_count = df_ips['country'].notnull().sum()
-        logging.info(
-            "Successfully matched %d IPs to countries.", matched_count)
-        return df_ips[['ip', 'company', 'action', 'country']]
+        try:
+            logging.info(
+                "Mapping IPs to countries for %d IP addresses.", len(df_ips))
+            df_ips['ip_int'] = df_ips['ip_address'].astype(int)
+            df_ips['country'] = df_ips['ip_int'].apply(lambda ip: next(
+                iter(self.tree[ip])).data if self.tree[ip] else None)
+            matched_count = df_ips['country'].notnull().sum()
+            logging.info(
+                "Successfully matched %d IPs to countries.", matched_count)
+            return df_ips[['user_id', 'signup_time', 'purchase_time', 'purchase_value', 'device_id', 'source', 'browser', 'sex', 'age', 'ip_address', 'country', 'class']]
+        except Exception as e:
+            logging.error("Error mapping IPs to countries: %s", str(e))
+            raise
